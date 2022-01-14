5581020abe2392cf19e06b6869d0fdb7567f94cc
Felix Soto <felixsotocas@gmail.com>
2022-01-14 00:12:57 -0700

    lista

--- src/app/ejercicios/monedas/monedas.page.ts
+++ src/app/ejercicios/monedas/monedas.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-monedas',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonedasPage implements OnInit {
 
-  constructor() { }
+  
+  //Variables calculos
+  public formula: string = "";
+  public resuelto: boolean = false;
 
-  ngOnInit() {
+  public dol: number;
+  public pes: number;
+  public conversion: string = "";
+
+
+  //Variables pesos
+  public conv: FormGroup;
+ 
+  constructor(
+    fb: FormBuilder
+  ) 
+  {
+    this.conv = fb.group({
+      pesosad: [null, [Validators.required]],
+      dolarpe: [null, [Validators.required]],
+
+    })
+  }
+  
+
+  public ngOnInit(): void { }
+
+  public cambiarFormula(evento): void{
+    this.formula = evento.detail.value;
+    this.reinicializar();
   }
 
+  public calcularDolaresap(): void{
+    const dol=20.3;
+    const dolarpe = this.conv.value.dolarpe;
+    this.conversion = ((dol*dolarpe)+"");
+    this.resuelto = true;
+  }
+
+  public calcularPesosad(): void{
+    const pes=.049;
+    const pesosad = this.conv.value.pesosad;
+    this.conversion= (pesosad*pes)+"";
+    this.resuelto = true;
+  }
+
+  private reinicializar(): void {
+
+
+    //reiniciar conv
+    this.conv.reset();
+
+
+    //Reiniciar globales
+    this.resuelto = false; 
+    this.conversion = "";
+  }
 }
